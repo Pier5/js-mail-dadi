7632afd53ef8c7c7f81c5879fb2a64a3136fd844
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-24 17:23:14 +0100

    -fixato- ora conferma mail presenti e viceversa

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,14 +1,21 @@
 let btnSend = document.querySelector('#btn');
 let validator = document.querySelector('#output');
 let validMail = ['pier@gmail.it', 'pg@gmail.it', 'piras@gmail.it', 'p_g@gmail.it', 'p5@gmail.it'];
-let searchName = document.querySelector('#mail-text').value;
+
 
 btnSend.addEventListener('click', function() {
+    let searchName = document.querySelector('#mail-text').value;
+    let found = false;
+
     for (let i = 0; i < validMail.length; i++) {
         if (validMail[i].toLowerCase() == searchName.toLowerCase()) {
+            found = true;
+        }
+
+        if (found == true) {
             validator.innerHTML = 'Mail confermata';
         } else {
-            validator.innerHTML = 'Mail non riconosciuta';
+            validator.innerHTML = 'Mail non trovata';
         }
     }
 })
